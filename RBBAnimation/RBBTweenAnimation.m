b290fd29d2e097c7bf5258caa5734bd3e6582a16
Robert Böhnke <robb@robb.is>
2013-10-20 19:24:51 +0200

    Add interpolation for CGPoint

--- RBBAnimation/RBBTweenAnimation.m
+++ RBBAnimation/RBBTweenAnimation.m
@@ -14,6 +14,7 @@
 
 + (id)tweenWithKeyPath:(NSString *)keyPath fromCGFloat:(CGFloat)from toCGFloat:(CGFloat)to interpolation:(RBBInterpolationFunction)interpolation;
 + (id)tweenWithKeyPath:(NSString *)keyPath fromCGRect:(CGRect)from toCGRect:(CGRect)to interpolation:(RBBInterpolationFunction)interpolation;
++ (id)tweenWithKeyPath:(NSString *)keyPath fromCGPoint:(CGPoint)from toCGPoint:(CGPoint)to interpolation:(RBBInterpolationFunction)interpolation;
 
 @end
 
@@ -36,6 +37,10 @@
 
     if (strcmp(from.objCType, @encode(CGRect)) == 0) {
         return [self tweenWithKeyPath:keyPath fromCGRect:[from CGRectValue] toCGRect:[to CGRectValue] interpolation:interpolation];
+    }
+
+    if (strcmp(from.objCType, @encode(CGPoint)) == 0) {
+        return [self tweenWithKeyPath:keyPath fromCGPoint:[from CGPointValue] toCGPoint:[to CGPointValue] interpolation:interpolation];
     }
 
     if ([from isKindOfClass:NSNumber.class]) {
@@ -65,6 +70,24 @@
         };
 
         return [NSValue valueWithCGRect:rect];
+    };
+
+    return [self animationWithKeyPath:keyPath block:block];
+}
+
++ (id)tweenWithKeyPath:(NSString *)keyPath fromCGPoint:(CGPoint)from toCGPoint:(CGPoint)to interpolation:(RBBInterpolationFunction)interpolation {
+    CGFloat deltaX = to.x - from.x;
+    CGFloat deltaY = to.y - from.y;
+
+    RBBAnimationBlock block = ^(CGFloat elapsed, CGFloat duration) {
+        CGFloat scaleFactor = interpolation(elapsed, duration);
+
+        CGPoint point = {
+            .x = from.x + scaleFactor * deltaX,
+            .y = from.y + scaleFactor * deltaY
+        };
+
+        return [NSValue valueWithCGPoint:point];
     };
 
     return [self animationWithKeyPath:keyPath block:block];
